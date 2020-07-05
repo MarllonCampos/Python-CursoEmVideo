d250e04e0f21c0dabdeae7b265c7157607b45aca
MarllonCampos <marlloncampos@outlook.com>
2020-07-05 02:51:17 -0300

    Adicionado pipfile e pipfile.lock??

--- ex003.py
+++ ex003.py
@@ -1,5 +1,4 @@
 print('====== DESAFIO 03 ======')
 n1 = int(input('Digite o primeiro numero = '))
 n2 = int(input('Digite o segundo numero = '))
-print('A soma e ', n1+ n2)
-0
+print('A soma e ', n1 + n2)
