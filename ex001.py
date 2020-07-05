a34c3c5b03d5d212e4854a39eb366225e044094b
MarllonCampos <marlloncampos@outlook.com>
2020-07-05 02:27:26 -0300

    Criado os tres primeiros exercicios do curso de python do CursoEmVideo

---
+++ ex001.py
@@ -0,0 +1,3 @@
+print('====== DESAFIO 01 ======')
+name = str(input('Qual e o seu nome? '))
+print('Ola '+ name + '! Prazer em te conhecer!' )
---
+++ ex003.py
@@ -0,0 +1,5 @@
+print('====== DESAFIO 03 ======')
+n1 = int(input('Digite o primeiro numero = '))
+n2 = int(input('Digite o segundo numero = '))
+print('A soma e ', n1+ n2)
+0
