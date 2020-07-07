e1c3e4c614597758347bbd72eb3cd9007a5fe0e9
MarllonCampos <marlloncampos@outlook.com>
2020-07-07 14:52:51 -0300

    Adicionado pacotes e corrigido erros de pipenv

---
+++ aulas/aula008a.py
@@ -0,0 +1,8 @@
+#   from math import sqrt,floor,ceil
+#   num = int(input('Digite um número: '))
+#   raiz = sqrt(num)
+#   print('A raiz de {} é igual a {}'.format(num,ceil(raiz)))
+
+import emoji
+
+print(emoji.emojize('Olá mundo :earth_americas:', use_aliases=True))
---
+++ exercicios/ex015.py
@@ -0,0 +1,9 @@
+import math as mt
+print('====== DESAFIO 15 ======')
+dias = int(input('Quantos dias o carro foi alugado: '))
+km = float(input('Quantos km foram rodados: '))
+precoDia = dias * 60
+precoKm = km * 0.15
+precoTotal = precoDia + precoKm
+
+print('O total a pagar é de R${:.2f}'.format(precoTotal))
