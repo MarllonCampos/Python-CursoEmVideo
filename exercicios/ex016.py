9c4c389c8045866694cb19a263f5a5dfc82a61d3
MarllonCampos <marlloncampos@outlook.com>
2020-07-07 15:07:24 -0300

    Exercicio 016 realizado

---
+++ exercicios/ex016.py
@@ -0,0 +1,7 @@
+from math import floor
+print('{:=^26}'.format(' Desafio 016 '))
+num = float(input('\nInsira um numero real: '))
+
+print('O número {} arredondado ficará {}\n'.format(num, floor(num)))
+
+print('{:=^26}'.format(' Desafio 016 '))
---
+++ exercicios/ex017.py
@@ -0,0 +1,6 @@
+print('{:=^26}'.format(' Desafio 017 '))
+
+co = float(input('Insira o valor do cateto oposto'))
+ca = float(input('Insira o valor do cateto adjacente'))
+
+print('{:=^26}'.format(' Desafio 017 '))
