d038e86b48a462e4c225b2d041c3048617eff207
MarllonCampos <marlloncampos@outlook.com>
2020-07-07 17:19:01 -0300

    Terminado exercicio 21 curso de python

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,7 +1,12 @@
-import playsound
+from pygame import mixer
 
 print('{:=^26}\n'.format(' Desafio 021 '))
 
-playsound.playsound('music/turn4what.mp3')
+mixer.init()
+mixer.music.load('turn4what.mp3')
+mixer.music.play()
+input('Pressione qualquer tecla para parar de tocar...')
+
+
 
 print('\n{:=^26}'.format(' Desafio 021 '))
