402d53bd00a3d5d5a18cda114483a25500117cfd
MarllonCampos <marlloncampos@outlook.com>
2020-07-11 01:06:53 -0300

    Terminado a aula 9 de python

--- aulas/aula009d.py
+++ aulas/aula009d.py
@@ -4,4 +4,16 @@ frase = 'Curso em Vídeo Python'
 
 #Função split() por padrão divide a string em seus espaços
 #Transformando onde foi separado em novas listas
-print('Agora é: {} 'frase.split())
+print('Original era: {}'.format(frase))
+print('Agora é: {} '.format(frase.split()))
+print('\n')
+
+
+
+#Junção de Strings
+
+#Metodo join é utilizado para juntar strings
+# '-'.join(frase) junta as diferentes listas separando por -
+print('Original era: {}'.format(frase))
+print('Com novas listas geradas fica: {}'.format(frase.split()))
+print('Utilizando o metodo join fica: {}'.format('****'.join(frase.split())))
